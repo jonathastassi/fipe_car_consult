261e9cfeaf5fe1dea4765ad09bcd57eec6baadeb
Jonathas Tassi <jonathastassi@hotmail.com>
2022-03-01 17:58:48 -0300

    AppLoadingLine created

--- lib/src/features/home/presenter/widgets/app_loading_line.dart
+++ lib/src/features/home/presenter/widgets/app_loading_line.dart
@@ -1,24 +1,31 @@
 import 'package:flutter/material.dart';
 
-class LoadingLine extends StatelessWidget {
+class AppLoadingLine extends StatelessWidget {
+  const AppLoadingLine({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(top: 15.0, bottom: 14.0),
+      padding: const EdgeInsets.only(
+        top: 15.0,
+        bottom: 14.0,
+      ),
       child: Row(
-        children: <Widget>[
+        children: const <Widget>[
           Expanded(
             child: Text(
               "Por favor, aguarde!",
-              style: TextStyle(fontSize: 16.0),
+              style: TextStyle(
+                fontSize: 16,
+              ),
             ),
           ),
-          Container(
-            width: 15.0,
-            height: 15.0,
+          SizedBox(
+            width: 15,
+            height: 15,
             child: CircularProgressIndicator(
               valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
-              strokeWidth: 2.0,
+              strokeWidth: 2,
             ),
           ),
         ],
